c8e3490ca4813a49b07b888380491d0beaf9af71
Eminokanju Oghenekparobor <66335632+oghenekparobor@users.noreply.github.com>
2023-04-20 20:10:57 +0100

    chore: portfolio updated

--- lib/subsessions/about.dart
+++ lib/subsessions/about.dart
@@ -25,29 +25,34 @@ class _AboutSubSectionState extends State<AboutSubSection> {
   var portfolio = [
     {
       'IMAGE':
-          'https://res.cloudinary.com/oghenekparobor/image/upload/v1655568636/my-portfolio/Screenshot_2022-06-18_at_17.03.48_lyzhdg.png',
-      'NAME': 'Chicken republic delta mall',
-      'URL':
-          'https://play.google.com/store/apps/details?id=com.chickenrepublicdeltamall',
-    },
-    {
-      'IMAGE':
-          'https://res.cloudinary.com/oghenekparobor/image/upload/v1655568628/my-portfolio/Screenshot_2022-06-18_at_17.04.11_sz3ufy.png',
-      'NAME': 'Cravings',
-      'URL': 'https://cravingsng.com/',
-    },
-    {
-      'IMAGE':
-          'https://res.cloudinary.com/oghenekparobor/image/upload/v1655568624/my-portfolio/Screenshot_2022-06-18_at_17.04.32_mro1pa.png',
+          'https://res.cloudinary.com/oghenekparobor/image/upload/v1662626024/my-portfolio/Screenshot_2022-06-18_at_17.04.32_mro1pa_gwahag.png',
       'NAME': 'Renmoney MFB',
       'URL':
           'https://play.google.com/store/apps/details?id=com.renmoney.android',
     },
     {
       'IMAGE':
+          'https://res.cloudinary.com/oghenekparobor/image/upload/v1682016362/my-portfolio/Screenshot_56_iv3nfa.png',
+      'NAME': 'Tafipal',
+      'URL': 'https://play.google.com/store/apps/details?id=com.tafipal.app',
+    },
+    {
+      'IMAGE':
+          'https://res.cloudinary.com/oghenekparobor/image/upload/v1662626959/my-portfolio/Screenshot_2022-06-18_at_17.04.11_sz3ufy_cek8l2.png',
+      'NAME': 'Cravings',
+      'URL': 'https://cravingsng.com/',
+    },
+    {
+      'IMAGE':
           'https://res.cloudinary.com/oghenekparobor/image/upload/v1655568637/my-portfolio/alerte_e6uwd5.jpg',
       'NAME': 'Alerte generic',
       'URL': 'https://play.google.com/store/apps/details?id=com.alerte.generic',
+    },
+    {
+      'IMAGE':
+          'https://res.cloudinary.com/oghenekparobor/image/upload/v1666136234/my-portfolio/unnamed_b3iz54.webp',
+      'NAME': 'Detailor',
+      'URL': 'https://play.google.com/store/apps/details?id=detailor.online',
     },
   ];
 
@@ -61,41 +66,34 @@ class _AboutSubSectionState extends State<AboutSubSection> {
             for (var i = 0; i < portfolio.length; i++)
               Padding(
                 padding: const EdgeInsets.only(bottom: 45),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Spacer(),
-                    Expanded(
-                      flex: 2,
-                      child: GestureDetector(
-                        onTap: () async => launchUrl(
-                          Uri.parse(portfolio[i]['URL']!),
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              portfolio[i]['NAME']!,
-                              style: GoogleFonts.abel(
-                                fontSize: 18,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Container(
-                              width: 400,
-                              height: 400,
-                              child: ProgressiveImage(
-                                width: 400,
-                                image: portfolio[i]['IMAGE']!,
-                                height: 400,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ],
+                child: GestureDetector(
+                  onTap: () async => launchUrl(
+                    Uri.parse(portfolio[i]['URL']!),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        portfolio[i]['NAME']!,
+                        style: GoogleFonts.abel(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Spacer(),
-                  ],
+                      SizedBox(height: 10),
+                      Container(
+                        width: 550,
+                        height: 600,
+                        margin: EdgeInsets.symmetric(horizontal: 30),
+                        child: FadeInImage.assetNetwork(
+                          placeholder: 'assets/placeholder.png',
+                          image: portfolio[i]['IMAGE']!,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Divider(),
+                    ],
+                  ),
                 ),
               ),
             SizedBox(height: 100),
